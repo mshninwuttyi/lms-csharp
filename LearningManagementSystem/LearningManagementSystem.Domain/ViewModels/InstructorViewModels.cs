7a601287aa85f432ad03a0df40c69f84013a29b4
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-02-23 11:48:38 +0700

    feat: Add Instructor View Model

---
+++ LearningManagementSystem/LearningManagementSystem.Domain/ViewModels/InstructorViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Domain.ViewModels
+{
+    public class InstructorViewModels
+    {
+        //Instructor
+        public string nrc { get; set; } = null!;
+        public string edu_background { get; set; } = null!;
+
+
+        //User
+        public string username { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public string password { get; set; } = null!;
+        public string phone { get; set; } = null!;
+        public DateOnly dob { get; set; }
+        public string address { get; set; } = null!;
+        public string profile_photo { get; set; } = null!;
+        public string role_id { get; set; } = null!; // Validation Required for allow only student or instructor
+        public bool is_available { get; set; } = false;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
